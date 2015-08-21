4c7cb1d8d9ea23c8f87602ab2479affd5932193e
infro <infro.j.z@gmail.com>
2015-08-20 20:02:30 -0700

    Fabric and Pixi javascript libraries

--- Test.Javascript.Image.Editor/App_Start/BundleConfig.cs
+++ Test.Javascript.Image.Editor/App_Start/BundleConfig.cs
@@ -8,10 +8,10 @@ namespace Test.Javascript.Image.Editor
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.UseCdn = true;
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
@@ -19,6 +19,14 @@ namespace Test.Javascript.Image.Editor
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle(
+                "~/bundles/fabricjs",
+                "http://cdnjs.cloudflare.com/ajax/libs/fabric.js/1.5.0/fabric.min.js"));
+
+            bundles.Add(new ScriptBundle(
+                "~/bundles/pixijs")
+                .Include("~/Scripts/pixi.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
