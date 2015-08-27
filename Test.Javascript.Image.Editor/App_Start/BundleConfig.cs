edf46f04e670b2767bb8e68ebc0e0571febb569c
infro <infro.j.z@gmail.com>
2015-08-26 17:24:56 -0700

    fab text

--- Test.Javascript.Image.Editor/App_Start/BundleConfig.cs
+++ Test.Javascript.Image.Editor/App_Start/BundleConfig.cs
@@ -1,8 +1,18 @@
-﻿using System.Web;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web;
 using System.Web.Optimization;
 
 namespace Test.Javascript.Image.Editor
 {
+    public class NonOrderingBundleOrderer : IBundleOrderer
+    {
+        public IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+        {
+            return files;
+        }
+    }
+
     public class BundleConfig
     {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
@@ -10,15 +20,32 @@ namespace Test.Javascript.Image.Editor
         {
             bundles.UseCdn = true;
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            var jQueryAndPlugins = new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/jquery.fontselect.js");
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            jQueryAndPlugins.Orderer = new NonOrderingBundleOrderer();
+
+            bundles.Add(jQueryAndPlugins);
+
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle(
+                "~/bundles/modernizr")
+                .Include("~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle(
+                "~/bundles/bootstrap")
+                .Include("~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle(
+                "~/bundles/webfontloader",
+                "https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js")
+                .Include("~/Scripts/webfont.js"));
+
+            bundles.Add(new ScriptBundle(
+                "~/bundles/underscore")
+                .Include("~/Scripts/underscore.js"));
 
             bundles.Add(new ScriptBundle(
                 "~/bundles/fabricjs",
@@ -27,11 +54,20 @@ namespace Test.Javascript.Image.Editor
 
             bundles.Add(new ScriptBundle(
                 "~/bundles/pixijs")
-                .Include("~/Scripts/pixi.js"));
+                .Include(
+                    "~/Scripts/pixi.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //bundles.Add(new ScriptBundle(
+            //    "~/bundles/")
+            //    .Include(
+            //        "~/Scripts/"));
+
+            bundles.Add(new StyleBundle(
+                "~/Content/css")
+                .Include(
+                    "~/Content/bootstrap.css",
+                    "~/Content/site.css",
+                    "~/Content/fontselect/fontselect.css"));
         }
     }
 }
